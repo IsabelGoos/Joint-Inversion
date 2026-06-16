39390813a92736417a50b0f435c5405138adc6cd
Yael Deniz <deni5876@vandals.uidaho.edu>
2026-06-16 11:06:18 -0700

    Yael's commit test, 4x4 digure in regroup_bins_DG_test

--- BinsStudy/scripts/regroup_bins_DG_test.py
+++ BinsStudy/scripts/regroup_bins_DG_test.py
@@ -21,14 +21,16 @@ n_enu_bins = 99
 min_bin_count = np.zeros((n_ct_bins, n_enu_bins))
 max_bin_count = np.zeros((n_ct_bins, n_enu_bins))
 
-for file in files[:1]: # Only test the first file for now
-    for channel in channels[:1]: # Only test the first channel for now
+fig, ax = plt.subplots(2,2, figsize=(10, 6))
+
+for i, file in enumerate(files): # Only test the last file for now
+    for j, channel in enumerate(channels): # Only test the first channel for now
         # create histogram
         histo        = SyntheticData(folder_data, file, channel)
         
         x, y, z      = histo.get_histo()
 
-        enu_regrouped, ct_regrouped, histo_regrouped = histo.regroup_bins_DG(n_pois_norm = 2)
+        enu_regrouped, ct_regrouped, histo_regrouped = histo.regroup_bins_DG(n_pois_norm = 25)
     
         print("*********************************************************************************************************************")
         print(" ")
@@ -36,25 +38,27 @@ for file in files[:1]: # Only test the first file for now
         print(np.shape(ct_regrouped))
         print(np.shape(histo_regrouped))
 
-        fig, ax = plt.subplots(figsize=(10, 6))
+        print("Check cols: ", histo_regrouped[:, 0])
 
-        pcm = ax.pcolormesh(enu_regrouped,    # x edges: shape (n_enu + 1,)
+  
+
+        pcm = ax[i,j].pcolormesh(enu_regrouped,    # x edges: shape (n_enu + 1,)
                             ct_regrouped,     # y edges: shape (n_ct  + 1,)
                             histo_regrouped,  # data:    shape (n_ct, n_enu)
                             norm=mcolors.LogNorm(),
                             cmap='viridis',
                             shading='flat')   # 'flat' = one color per cell, requires edges
 
-        cbar = fig.colorbar(pcm, ax=ax)
+        cbar = fig.colorbar(pcm, ax=ax[i,j], pad=0.01)
         cbar.set_label(channel, fontsize=12)
 
-        ax.set_xscale('log')
-        ax.set_xlabel('Neutrino Energy [GeV]', fontsize=12)
-        ax.set_ylabel(r'$\cos\theta$', fontsize=12)
-        ax.set_title(f"Regrouped — {channel}\nshape: {histo_regrouped.shape}")
+        ax[i,j].set_xscale('log')
+        ax[i,j].set_xlabel('Neutrino Energy [GeV]', fontsize=12)
+        ax[i,j].set_ylabel(r'$\cos\theta$', fontsize=12)
+        ax[i,j].set_title(f"Regrouped — {file}/{channel}\nshape: {histo_regrouped.shape}")
 
-        plt.tight_layout()
-        plt.show()
+plt.tight_layout()
+plt.show()
 
 
 
