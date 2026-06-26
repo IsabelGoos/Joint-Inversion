0669d55ee17c0baca7ca585948ab6ce749027894
IsabelGoos <isabel.astrid.goos.bosco@gmail.com>
2026-06-26 14:23:54 +0200

    Nu-flux

---
+++ Neutrino-Flux/scripts/read_neutrino_flux.jl
@@ -0,0 +1,9 @@
+
+
+
+nuflux = readdlm("nuflux.txt") # default daemonflux
+nuflux = reshape(nuflux, 100, 100, 5)
+nuflux_νe = nuflux[:,:,4]
+nuflux_νμ = nuflux[:,:,2]
+nuflux_antiνe = nuflux[:,:,5]
+nuflux_antiνμ = nuflux[:,:,3]
