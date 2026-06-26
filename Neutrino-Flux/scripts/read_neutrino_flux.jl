b7f21bf4e22db7bee9050d57d182afe78165589f
IsabelGoos <isabel.astrid.goos.bosco@gmail.com>
2026-06-26 15:25:27 +0200

    Read nuflux

--- Neutrino-Flux/scripts/read_neutrino_flux.jl
+++ Neutrino-Flux/scripts/read_neutrino_flux.jl
@@ -1,9 +1,11 @@
+using CSV
+using DataFrames
 
 
+#nuflux = readdlm("nuflux.txt") # default daemonflux
+#nuflux = reshape(nuflux, 100, 100, 5)
+#nuflux_νe = nuflux[:,:,4]
+#nuflux_νμ = nuflux[:,:,2]
+#nuflux_antiνe = nuflux[:,:,5]
+#nuflux_antiνμ = nuflux[:,:,3]
 
-nuflux = readdlm("nuflux.txt") # default daemonflux
-nuflux = reshape(nuflux, 100, 100, 5)
-nuflux_νe = nuflux[:,:,4]
-nuflux_νμ = nuflux[:,:,2]
-nuflux_antiνe = nuflux[:,:,5]
-nuflux_antiνμ = nuflux[:,:,3]
