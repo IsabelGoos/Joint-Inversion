3045aff33a46c996f1f12381f62d683c9f75e4d1
IsabelGoos <isabel.astrid.goos.bosco@gmail.com>
2026-06-29 17:13:14 +0200

    Work on the cross-sections module

--- Forward-Model/Full-Pipeline/scripts/full_pipeline.jl
+++ Forward-Model/Full-Pipeline/scripts/full_pipeline.jl
@@ -1,6 +1,16 @@
 include("../../Neutrino-Flux/scripts/create_neutrino_flux.jl")
 using .Neutrino_Flux
+include("../../Neutrino-Interactions/scripts/create_neutrino_cross_sections.jl")
+using .Neutrino_Cross_Sections
+
+energies = logrange(1, 100, 100)
 
 # read the neutrino-flux table nuflux.csv given in .../Neutrino-Flux/data
-nu_νe, nu_νμ, nu_antiνe, nu_antiνμ = read_neutrino_flux_table("nuflux", 100, 100, false)
+flux_νe, flux_νμ, flux_antiνe, flux_antiνμ = read_neutrino_flux_table("nuflux", 100, 100, false)
+
+# read the neutrino cross sections from cross_sections.json in .../Neutrino-Interactions/data
+cs_νe_CC, cs_νμ_CC, cs_ντ_CC, cs_antiνe_CC, cs_antiνμ_CC, cs_antiντ_CC = read_neutrino_cross_sections_info("cross_sections.json")
+cs_νe_CC = evaluate.(Ref(cs_νe_CC),  log10.(energies)) ./ energies)
+
+
 
--- Forward-Model/Neutrino-Interactions/scripts/create_neutrino_cross_sections.jl
+++ Forward-Model/Neutrino-Interactions/scripts/create_neutrino_cross_sections.jl
@@ -1,8 +1,25 @@
+module Neutrino_Cross_Sections
+export read_neutrino_cross_sections_info, evaluate_cubicspline
+
 using JSON
 
-module Neutrino_Cross_Sections
+"""
+    CubicSpline
 
-function read_neutrino_cross_sections(filename::String)
+Holds the knots (k) and coefficient matrix (c) for a SciPy-generated cubic spline.
+"""
+struct CubicSpline
+    k::Vector{Float64}  # spline knots 
+    c::Matrix{Float64}  # spline polynomial coefficients
+end
+
+"""
+    read_neutrino_cross_sections_info(filename::String)
+
+Reads cross-section data from a file in the `../data` directory and 
+returns a tuple with the information for all 6 interesting neutrino interaction channels.
+"""
+function read_neutrino_cross_sections_info(filename::String)
 
     # construct path
     data_dir  = joinpath(@__DIR__, "..", "data")
@@ -11,51 +28,65 @@ function read_neutrino_cross_sections(filename::String)
     # read the data
     if endswith(filename, ".json")
         data = JSON.parsefile(data_path)
+        # parse JSON dictionary 
+        cross_section_splines = Dict{String, CubicSpline}()
+        for (key, val) in data
+            k = Float64.(val["k"])
+            c_list = val["c"]                
+            c = Float64.(hcat(c_list...))'   
+            cross_section_splines[key] = CubicSpline(k, c)
+        end
+        # extract specific neutrino interaction channels
+        νe_CC  = cross_section_splines["nue_CC_logE"]
+        νμ_CC  = cross_section_splines["num_CC_logE"]
+        ντ_CC  = cross_section_splines["nut_CC_logE"]
+        antiνe_CC = cross_section_splines["anue_CC_logE"]
+        antiνμ_CC = cross_section_splines["anum_CC_logE"]
+        antiντ_CC = cross_section_splines["anut_CC_logE"]
+    else
+        error("Unsupported file format: '$filename'.")
     end
 
-    struct CubicSpline_Info
-        # knots
-        k::Vector{Float64}
-        # coefficients
-        c::Matrix{Float64}
-    end
+    return νe_CC, νμ_CC, ντ_CC, antiνe_CC, antiνμ_CC, antiντ_CC
 
-    cross_section_splines = Dict{String, CubicSpline_Info}()
-    for (key, val) in data
-        k = Float64.(val["k"])
-        c_list = val["c"]                
-        c = Float64.(hcat(c_list...))'   
-        cross_section_splines[key] = CubicSpline_Info(k, c)
-    end
-
-    nue_CC  = cross_section_splines["nue_CC_logE"]
-    num_CC  = cross_section_splines["num_CC_logE"]
-    nut_CC  = cross_section_splines["nut_CC_logE"]
-    anue_CC = cross_section_splines["anue_CC_logE"]
-    anum_CC = cross_section_splines["anum_CC_logE"]
-    anut_CC = cross_section_splines["anut_CC_logE"]
-
-    return nue_CC, num_CC, nut_CC, anue_CC, anum_CC, anut_CC
-
-end
-
-end
-
+end 
 
 """
+    evaluate_cubicspline(s::CubicSpline, x::Float64)
 
-function evaluate(s::CubicSplineSciPy, x::Float64)
+Evaluate the cubic spline `s` at a given coordinate `x`.
+
+# Arguments
+- `s::CubicSpline`: The struct containing the spline knots and coefficients.
+- `x::Float64`: The value at which to evaluate the spline.
+
+# Returns
+- `Float64`: The interpolated value.
+"""
+function evaluate_cubicspline(s::CubicSpline, x::Float64)
+
     k = s.k
     c = s.c
-    # find interval
+
+    # search in which interval 'x' falls into
     i = searchsortedlast(k, x)
+
+    # if x is beyond the last knot, use the final valid polynomial segment
     if i >= length(k)
         i = length(k) - 1
+    # if x is below the first knot, use the first valid polynomial segment
     elseif i < 1
         i = 1
     end
+
+    # distance from the left knot of the current interval
     dx = x - k[i]
-    return ((c[1,i]*dx + c[2,i])*dx + c[3,i])*dx + c[4,i]
+
+    # valuate the cubic polynomial using Horner's method
+    return ((c[1, i] * dx + c[2, i]) * dx + c[3, i]) * dx + c[4, i]
 end
 
-"""
+end
+
+
+
