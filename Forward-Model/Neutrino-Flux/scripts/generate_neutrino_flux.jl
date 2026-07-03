fb2e88c839b0f4aaa348f81fc0abecda4e2dfb6d
IsabelGoos <isabel.astrid.goos.bosco@gmail.com>
2026-07-03 16:05:02 +0200

    Added an interpolator

--- Forward-Model/Neutrino-Flux/scripts/generate_neutrino_flux.jl
+++ Forward-Model/Neutrino-Flux/scripts/generate_neutrino_flux.jl
@@ -3,6 +3,7 @@ export read_neutrino_flux_table
 
 using CSV
 using DataFrames
+using Interpolations
 
 """
     read_neutrino_flux_table(filename::String, nEbins::Int, nθbins::Int, has_header::Bool)
@@ -43,12 +44,49 @@ function read_neutrino_flux_table(filename::String, nEbins::Int, nθbins::Int, h
     flux_3d = reshape(Matrix(raw_df), nEbins, nθbins, 5)
  
     # Slice the 3D grid to extract the 2D planes for each specific flavor
+    energies      = flux_3d[:, 1, 1]
     nuflux_νμ     = flux_3d[:, :, 2]
     nuflux_antiνμ = flux_3d[:, :, 3]
     nuflux_νe     = flux_3d[:, :, 4]
     nuflux_antiνe = flux_3d[:, :, 5]
 
-    return nuflux_νe, nuflux_νμ, nuflux_antiνe, nuflux_antiνμ
+    # Compute values at bin centers
+    bin_centers, nuflux_νμ_interp = interpolate_flux_at_bin_centers(energies, nuflux_νμ, true)
+    -, nuflux_antiνμ_interp = interpolate_flux_at_bin_centers(energies, nuflux_antiνμ, true)
+    -, nuflux_νe_interp     = interpolate_flux_at_bin_centers(energies, nuflux_νe, true)
+    -, nuflux_antiνe_interp = interpolate_flux_at_bin_centers(energies, nuflux_antiνe, true)
+
+    return bin_centers, nuflux_νe_interp, nuflux_νμ_interp, nuflux_antiνe_interp, nuflux_antiνμ_interp, energies, nuflux_νe, nuflux_νμ, nuflux_antiνe, nuflux_antiνμ
+
+end
+
+"""
+    intepolate_flux_at_bin_centers(energies::Vector{Float64}, flux::Matrix{Float64}, energies_in_log::Bool)
+
+Calculates energy bin centers, interpolates the log(flux) at log(energies).
+
+# Returns
+A tuple containing
+1. bin centers
+2. interpolated flux values evaluated at those centers
+"""
+function interpolate_flux_at_bin_centers(energies::Vector{Float64}, flux::Matrix{Float64}, energies_in_log::Bool)
+
+    # create 1D coordinate array for the y axis
+    angles_indices = 1.0:size(flux, 2)
+
+    # calculate the bin centers    
+    if energies_in_log == true
+        bin_centers = sqrt.(energies[1:end-1] .* energies[2:end]) 
+    else
+        bin_centers = 0.5 .* (energies[1:end-1] .+ energies[2:end])
+    end
+
+    # we interpolate log(flux) vs log(energies)
+    interpolators = [interpolate(log.(energies), log.(col), FritschCarlsonMonotonicInterpolation()) for col in eachcol(flux)]
+    # evaluate the interpolator at the log of the bin centers, then exponentiate back
+    interpolated_flux = exp.(hcat([interp.(log.(bin_centers)) for interp in interpolators]...))   
+    return bin_centers, interpolated_flux
 
 end
 
