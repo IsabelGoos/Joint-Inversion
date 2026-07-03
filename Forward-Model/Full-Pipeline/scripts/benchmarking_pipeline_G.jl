5501ba3c127f1cb2e2ddbd508278c85b8221ec3b
IsabelGoos <isabel.astrid.goos.bosco@gmail.com>
2026-07-03 11:07:08 +0200

    Improved my benchmarking pipeline

--- Forward-Model/Full-Pipeline/scripts/benchmarking_pipeline_G.jl
+++ Forward-Model/Full-Pipeline/scripts/benchmarking_pipeline_G.jl
@@ -4,43 +4,45 @@ include("../../Neutrino-Interactions/scripts/create_neutrino_cross_sections.jl")
 using .Neutrino_Cross_Sections
 using Plots
 using Interpolations
+using LaTeXStrings
 
 energies = logrange(1, 100, 100)
 # read the neutrino-flux table nuflux.csv given in .../Neutrino-Flux/data
-flux_νe, flux_νμ, flux_antiνe, flux_antiνμ = read_neutrino_flux_table("nuflux", 100, 100, false)
+flux_νe, flux_νμ, flux_antiνe, flux_antiνμ = Neutrino_Flux.read_neutrino_flux_table("nuflux", 100, 100, false)
 # neutrino fluxes look good
 # (comparing with Figure 3 in 2arXiv:1502.03916;
 # there are slight differences, especially at low energies, because
 # daemonflux is a bit different from honda)
-p1 = plot(energies, (energies.^3) .* sum(flux_νe,     dims=2) ./ 99, label="νe", 
+p1 = plot(energies, (energies.^3) .* sum(flux_νe,     dims=2) ./ 99, label=L"$\nu_e$", 
           xaxis=:log, yaxis=:log, legend=:bottom,
           xlabel="E/GeV", ylabel="(σ/E) 10^{-42} m^2/Gev")
-plot!(p1, energies, (energies.^3) .* sum(flux_νμ,     dims=2) ./ 99, label="νμ")
-plot!(p1, energies, (energies.^3) .* sum(flux_antiνe, dims=2) ./ 99, label="antiνe")
-plot!(p1, energies, (energies.^3) .* sum(flux_antiνμ, dims=2) ./ 99, label="antiνμ")
+plot!(p1, energies, (energies.^3) .* sum(flux_νμ,     dims=2) ./ 99, label=L"$\nu_\mu$")
+plot!(p1, energies, (energies.^3) .* sum(flux_antiνe, dims=2) ./ 99, label=L"$\bar{\nu}_e$")
+plot!(p1, energies, (energies.^3) .* sum(flux_antiνμ, dims=2) ./ 99, label=L"$\bar{\nu}_\mu$")
 
 
 
 energies = logrange(0.1, 100, 100)
 # read the neutrino cross sections from cross_sections.json in .../Neutrino-Interactions/data
-cs_νe_CC, cs_νμ_CC, cs_ντ_CC, cs_antiνe_CC, cs_antiνμ_CC, cs_antiντ_CC = read_neutrino_cross_sections_info("cross_sections.json")
-cs_νe_CC     = evaluate_cubicspline.(Ref(cs_νe_CC), log10.(energies))
-cs_νμ_CC     = evaluate_cubicspline.(Ref(cs_νμ_CC), log10.(energies))
-cs_ντ_CC     = evaluate_cubicspline.(Ref(cs_ντ_CC), log10.(energies))
-cs_antiνe_CC = evaluate_cubicspline.(Ref(cs_antiνe_CC), log10.(energies))
-cs_antiνμ_CC = evaluate_cubicspline.(Ref(cs_antiνμ_CC), log10.(energies))
-cs_antiντ_CC = evaluate_cubicspline.(Ref(cs_antiντ_CC), log10.(energies))
+cs_νe_CC, cs_νμ_CC, cs_ντ_CC, cs_antiνe_CC, cs_antiνμ_CC, cs_antiντ_CC = Neutrino_Cross_Sections.read_neutrino_cross_sections_info("cross_sections.json")
+cs_νe_CC     = Neutrino_Cross_Sections.evaluate_cubicspline.(Ref(cs_νe_CC), log10.(energies))
+cs_νμ_CC     = Neutrino_Cross_Sections.evaluate_cubicspline.(Ref(cs_νμ_CC), log10.(energies))
+cs_ντ_CC     = Neutrino_Cross_Sections.evaluate_cubicspline.(Ref(cs_ντ_CC), log10.(energies))
+cs_antiνe_CC = Neutrino_Cross_Sections.evaluate_cubicspline.(Ref(cs_antiνe_CC), log10.(energies))
+cs_antiνμ_CC = Neutrino_Cross_Sections.evaluate_cubicspline.(Ref(cs_antiνμ_CC), log10.(energies))
+cs_antiντ_CC = Neutrino_Cross_Sections.evaluate_cubicspline.(Ref(cs_antiντ_CC), log10.(energies))
 # cross sections per nucleon look good 
 # (comparing with EarthProbe/ExtModels/xsection/crossSection.root and
 # with Figure 9 (top and bottom) in arXiv:1305.7513)
-p2 = plot(energies, cs_νe_CC,     label="νe CC", xaxis=:log,
+p2 = plot(energies, cs_νe_CC,     label=L"$\nu_e$ CC", xaxis=:log,
           xlabel="E/GeV", ylabel="(σ/E) 10^{-42} m^2/Gev")
-plot!(p2, energies, cs_νμ_CC,     label="νμ CC")
-plot!(p2, energies, cs_ντ_CC,     label="ντ CC")
-plot!(p2, energies, cs_antiνe_CC, label="anti-νe CC")
-plot!(p2, energies, cs_antiνμ_CC, label="anti-νμ CC")
-plot!(p2, energies, cs_antiντ_CC, label="anti-ντ CC")
+plot!(p2, energies, cs_νμ_CC,     label=L"$\nu_\mu$ CC")
+plot!(p2, energies, cs_ντ_CC,     label=L"$\nu_\tau$ CC")
+plot!(p2, energies, cs_antiνe_CC, label=L"$\bar{\nu}_e$ CC")
+plot!(p2, energies, cs_antiνμ_CC, label=L"$\bar{\nu}_\mu$ CC")
+plot!(p2, energies, cs_antiντ_CC, label=L"$\bar{\nu}_\tau$ CC")
 
-# read the Neutrino Oscillation Probabilities from Neurthino.jl.../Oscillation-Probabilities/data
-energies = logrange(0.1, 100, 100) #GeV 
- 
+
+
+energies = logrange(0.1, 100, 100)
+# read the Neutrino Osicllation Probabilities calculated using Neutrhino in .../Neutrino-Probabilities/data
