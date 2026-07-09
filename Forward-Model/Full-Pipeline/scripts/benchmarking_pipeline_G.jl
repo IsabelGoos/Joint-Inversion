38c4bd6d77d17adba835e99e7b445a7528b4d784
IsabelGoos <isabel.astrid.goos.bosco@gmail.com>
2026-07-09 14:43:18 +0200

    Added oscillation probabilities part and first try of interacting events computation

--- Forward-Model/Full-Pipeline/scripts/benchmarking_pipeline_G.jl
+++ Forward-Model/Full-Pipeline/scripts/benchmarking_pipeline_G.jl
@@ -1,5 +1,8 @@
 include("../../Neutrino-Flux/scripts/generate_neutrino_flux.jl")
 using .Neutrino_Flux
+ParamFile = "../config/testparam.csv" # TO-DO: adapt this path, in case it changes 
+include("../../Neutrino-Oscillations/scripts/generate_oscillation_probabilities.jl")
+using .Neutrino_Oscillations
 include("../../Neutrino-Interactions/scripts/generate_neutrino_cross_sections.jl")
 using .Neutrino_Cross_Sections
 using Plots
@@ -38,8 +41,97 @@ plot!(p2, bin_centers, 100 .* (sum(flux_antiνμ_interp, dims=2) .- flux_antiν�
 
 
 
-energies = logrange(0.1, 100, 100)
-# read the neutrino osicllation probabilities calculated using Neutrhino
+# neutrino oscillation probabilities look good, maybe Paνe2aντ deserves some attention 
+minX, maxX, nX = -6500e3, 6500e3, 521
+n_angles  = 100
+n_pts     = 100
+zposition = 2.5e3
+energy_min = 1.0
+energy_max = 40.0
+osc_probs_nu, osc_probs_antinu = Neutrino_Oscillations.produce_neutrino_oscillation_probabilities(minX, maxX, nX, n_angles, n_pts, zposition, energy_min=energy_min, energy_max=energy_max)
+# neutrinos, NMO
+Pνe2νe = osc_probs_nu[:, :, 1, 1]' 
+Pνe2νμ = osc_probs_nu[:, :, 1, 2]' 
+Pνe2ντ = osc_probs_nu[:, :, 1, 3]' 
+Pνμ2νe = osc_probs_nu[:, :, 2, 1]' 
+Pνμ2νμ = osc_probs_nu[:, :, 2, 2]' 
+Pνμ2ντ = osc_probs_nu[:, :, 2, 3]' 
+# antineutrinos, NMO
+Paνe2aνe = osc_probs_antinu[:, :, 1, 1]' 
+Paνe2aνμ = osc_probs_antinu[:, :, 1, 2]' 
+Paνe2aντ = osc_probs_antinu[:, :, 1, 3]' 
+Paνμ2aνe = osc_probs_antinu[:, :, 2, 1]' 
+Paνμ2aνμ = osc_probs_antinu[:, :, 2, 2]' 
+Paνμ2aντ = osc_probs_antinu[:, :, 2, 3]' 
+
+# plots for neutrinos, NMO
+angles = range(-1, 0, length=100)
+titles = (
+    L"P($\nu_e \rightarrow \nu_e$)", L"P($\nu_e \rightarrow \nu_\mu$)", L"P($\nu_e \rightarrow \nu_\tau$)",
+    L"P($\nu_\mu \rightarrow \nu_e$)", L"P($\nu_\mu \rightarrow \nu_\mu$)", L"P($\nu_\mu \rightarrow \nu_\tau$)"
+)
+all_probs = (
+    Pνe2νe, Pνe2νμ, Pνe2ντ,
+    Pνμ2νe, Pνμ2νμ, Pνμ2ντ
+)
+plot_list = []
+for idx in 1:6
+    # determine grid positions for labels
+    is_bottom_row = idx > 3
+    is_left_column = (idx == 1 || idx == 4)
+    is_right_column = (idx ==3 || idx == 6)
+    p = heatmap(
+        energies, angles, all_probs[idx],              
+        title = titles[idx],
+        xlabel = is_bottom_row ? "Energy/GeV" : "",
+        ylabel = is_left_column ? L"\cos\theta" : "",
+        colorbar_title = is_right_column ? "Oscillation probability" : "",
+        xscale = :log10,
+        clim = (0, 1),
+        cmap = :viridis
+    )
+    push!(plot_list, p)
+end
+final_plot = plot(
+    plot_list..., 
+    layout = (2, 3), 
+    size = (1000, 400), 
+    margin = 1Plots.mm,
+)
+
+# plots for antineutrinos, NMO
+titles = (
+    L"P($\bar{\nu}_e \rightarrow \bar{\nu}_e$)", L"P($\bar{\nu}_e \rightarrow \bar{\nu}_\mu$)", L"P($\bar{\nu}_e \rightarrow \bar{\nu}_\tau$)",
+    L"P($\bar{\nu}_\mu \rightarrow \bar{\nu}_e$)", L"P($\bar{\nu}_\mu \rightarrow \bar{\nu}_\mu$)", L"P($\bar{\nu}_\mu \rightarrow \bar{nu}_\tau$)"
+)
+all_probs = (
+    Paνe2aνe, Paνe2aνμ, Paνe2aντ,
+    Paνμ2aνe, Paνμ2aνμ, Paνμ2aντ
+)
+plot_list = []
+for idx in 1:6
+    # determine grid positions for labels
+    is_bottom_row = idx > 3
+    is_left_column = (idx == 1 || idx == 4)
+    is_right_column = (idx ==3 || idx == 6)
+    p = heatmap(
+        energies, angles, all_probs[idx],              
+        title = titles[idx],
+        xlabel = is_bottom_row ? "Energy/GeV" : "",
+        ylabel = is_left_column ? L"\cos\theta" : "",
+        colorbar_title = is_right_column ? "Oscillation probability" : "",
+        xscale = :log10,
+        clim = (0, 1),
+        cmap = :viridis
+    )
+    push!(plot_list, p)
+end
+final_plot = plot(
+    plot_list..., 
+    layout = (2, 3), 
+    size = (1000, 400), 
+    margin = 1Plots.mm
+)
 
 
 
